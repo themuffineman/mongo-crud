3c3968d8950fcc337d4864f9ed3da76949f8b847
Petrus <petrusheya@gmail.com>
2024-02-13 22:09:25 +0200

    Dashboard page scripts

--- app/api/userdata/[userId]/route.js
+++ app/api/userdata/[userId]/route.js
@@ -1,17 +1,12 @@
 import { connectToMongoDB } from "@/utils/mongoDBConnect";
 import { UserDataModel } from "@/utils/userDataSchema";
 import { NextResponse } from "next/server";
-import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+
 
 
 export const GET = async ({ params })=>{
-   const { isAuthenticated} = getKindeServerSession();
 
     try {
-        const authenticated = await isAuthenticated()
-        if(!authenticated){
-            throw new Error({message:'User Is Not Authenticated', status: 401})
-        }
         await connectToMongoDB()
         const userData = await UserDataModel.findOne({ authUserId: params.userId })
         if (!userData){
--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,9 +1,35 @@
 "use client"
 import Note from '@/components/note'
-import React, {useContext} from 'react'
+import React, {useState, useEffect} from 'react'
+import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from 'next/navigation'
 
-const page = () => {
-    const user = useContext(UserDataContext)
+const page = async () => {
+    
+
+   const { isAuthenticated, getUser} = getKindeServerSession();
+   const [userData, setUserData] = useState()
+   const user = await getUser();
+   console.log(user)
+
+   if(!(await isAuthenticated())){
+    redirect('/api/auth/login')
+   }
+
+    useEffect(()=>{
+        const getUserData = async ()=>{
+            try {
+                const apiRes = await fetch(`http://localhost:3000/api/userdata/${user.id}`)
+                const userDataObject = await apiRes.json()
+                setUserData(userDataObject)
+
+            } catch (error) {
+                console.error('Error Fetching User Data:', error)
+            }
+        }
+        getUserData()
+    }, [])
+
   return (
     <div className='w-full h-screen flex items-center justify-between '>
         <div className='h-full w-[15%] bg-white border-r border-gray-100 relative flex flex-col justify-between items-center'>
@@ -15,7 +41,7 @@ const page = () => {
                 <h3 className=' cursor-pointer text-left hover:bg-gray-100 pl-3 py-3 w-full rounded-sm text-black font-semibold tracking-tighter'>Deleted Notes</h3>
                 <h3 className=' cursor-pointer text-left hover:bg-gray-100 pl-3 py-3 w-full rounded-sm text-black font-semibold tracking-tighter'>Completed Notes</h3>
             </div>
-            <div className="mb-4 bg-gray-100 w-40 rounded-md text-black font-bold p-5 tracking-tighter">Profile</div>
+            <div className="mb-4 bg-gray-100 w-40 rounded-md text-black font-bold p-5 tracking-tighter">Welcome {user.given_name}</div>
         </div>
         <div className='h-full p-10 pt-4 w-[85%] bg-gray-100 flex flex-col'>  
             <div className='w-full flex items-center justify-between'>
@@ -23,7 +49,7 @@ const page = () => {
                 <button className='bg-black p-3 text-white rounded-sm hover:bg-neutral-700'>Create a new note</button>
             </div> 
             <div className='w-full scroll-smooth grid grid-flow-row justify-items-center self-center overflow-scroll overflow-x-hidden p-5 gap-5'>
-                {user.map((note)=> <Note noteData={note}/>)}
+                {/* {user.map((note)=> <Note noteData={note}/>)} */}
             </div>
         </div>
     </div>
