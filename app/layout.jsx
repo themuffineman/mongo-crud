b0e62a04345f61901d9cc76a6bc44e52c9a15829
Petrus <petrusheya@gmail.com>
2024-02-18 23:57:54 +0200

    Uncommented imports

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,10 +1,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-// import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
-// import { Button } from "@/components/ui/button";
-// import {LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import {RegisterLink, LoginLink} from "@kinde-oss/kinde-auth-nextjs/components";
+import { Button } from "@/components/ui/button";
+import {LogoutLink} from "@kinde-oss/kinde-auth-nextjs/components";
 import {getKindeServerSession} from "@kinde-oss/kinde-auth-nextjs/server";
-// import Loading from "./loading";
+import Loading from "./loading";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +20,7 @@ export default async function RootLayout({ children }) {
       <body className={inter.className}>
         <main className="flex min-h-screen h-screen w-screen min-w-screen flex-col items-center justify-center">
         {children}
-        {/* {isLoading && <Loading/> } */}
+        {isLoading && <Loading/> }
         </main>
       </body>
     </html>
